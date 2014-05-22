332fdcb7fe680903e35e3b9de0fd7c01b5df3f61
gpestana <g6pestana@gmail.com>
2014-05-22 11:33:40 +0300

    parse done. creating html

---
+++ example/example.md
@@ -0,0 +1,12 @@
+:title		Example title
+:date		10.01.2013 
+:slug		example1
+
+This blog generatos uses markdown sintax. For more information check [here](https://daringfireball.net/projects/markdown/). 
+Markdown sintax is pretty cool since it allows to write rich documents, emphasizing readability above all. 
+---
+:title		Post2 
+:date		11.02.2013 
+:slug		example-post2 
+
+The posts don't need to be sorted in this document. By default, the list of posts will be sorted based on the date provided in the metadata. 
--- src/davinci.js
+++ src/davinci.js
@@ -1,14 +1,13 @@
 var fs 		= require('fs'),	
 argv	 	= require('minimist')(process.argv.slice(2))
 
-
 /*
 todo:
 - error handling with execptions
 - log processing
-- all 
+- refactoring regex parsing 
+- sort posts by date
 */
-
 
 var genFile = argv._[0]
 var output = argv._[1]
@@ -28,10 +27,61 @@ var content = fs.readFile(process.cwd()+'/'+genFile, function(err, data) {
 		console.log('Error: not possible to open content file')
 		process.exit(1)
 	}
-	generate(data.toString())
+	parseDocument(data.toString())
 })
 
+function parseDocument(data) {
+	objPosts = []
+	rawPosts = data.split('---')
 
-function generate(data) {
-	console.log(data)
+	rawPosts.forEach(function(rawPost) {
+		objPosts.push(parseRaw(rawPost))
+	})
+
+	generateHTML(objPosts)
+}
+
+
+//#todo: refactoring + support variable metadata types
+function parseRaw(rawPost) {
+	var finalObj = {}
+	var regRes
+
+	//after: /\s+\S*$/, ""
+	//before: /^[\s]*/,''
+
+	regRes = rawPost.match(/:title\t.(.*)/)
+	rawPost = rawPost.replace(/:title(.*)./, '')
+	finalObj.title = regRes[1]
+	
+	regRes = rawPost.match(/:date\t.(.*)/)
+	rawPost = rawPost.replace(/:date(.*)./, '')
+	finalObj.date = regRes[1]
+
+	regRes = rawPost.match(/:slug\t.(.*)/)
+	rawPost = rawPost.replace(/:slug(.*)./, '')
+
+	finalObj.slug = regRes[1].replace(/\s+\S*$/, "")
+	finalObj.content = rawPost.replace(/^[\s]*/,'')
+
+	return finalObj
+}
+
+
+function generateHTML(objPosts) {
+	objPosts.forEach(function(postData) {
+		var htmlFile = output+'/'+postData.slug+'.html'
+		
+		//wrap content with template
+		fs.readFile('example/template.html', function() {
+			finalContent = finalContent.replace("{{"+key+"}}", entry[key]);
+		})
+
+		fs.writeFile(htmlFile, postData.content, function(err) {
+			if(err) {
+				throw err
+			}
+			console.log('>> '+postData.title+' written.')
+		})
+	})
 }
